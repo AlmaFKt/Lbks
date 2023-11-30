4afe8ef9462087c0270a845082d02f63250f9d17
AlmaFKt <almafloresram@gmail.com>
2023-11-29 22:31:33 -0600

    FINISHED!

--- lib/Theme/ligth_mode.dart
+++ lib/Theme/ligth_mode.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData LigthMode = ThemeData(
     brightness: Brightness.light,
     colorScheme: ColorScheme.light(
-        background: Color.fromARGB(255, 252, 222, 232),
+        background: Color.fromARGB(255, 252, 248, 249),
         primary: Color.fromARGB(255, 14, 13, 13),
         secondary: Color.fromARGB(255, 12, 12, 12),
         inversePrimary: Color.fromARGB(255, 243, 213, 213)),
