95028dec72e352bcbbe8dada18e5c7d8a0fb4952
AlmaFKt <almafloresram@gmail.com>
2024-01-18 23:59:27 -0600

    Remove unused imports and dependencies

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -3,8 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:icons_plus/icons_plus.dart';
-
 import 'home_page.dart';
 
 class ProfilePage extends StatefulWidget {
